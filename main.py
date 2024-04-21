a246d113634eaf18a7d450c7dd373e3b06c3acbc
nplanes <nasedkin_n22@mail.ru>
2024-04-21 18:47:34 +0300

    model

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
+    user = User()
+    user.name = "Пользователь 1"
+    user.about = "биография пользователя 1"
+    user.email = "email@email.ru"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
     app.run()
 
 
