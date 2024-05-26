4114a42792535c55596c889958e16f04580dbcfe
mooslyaka <mooslyakaboowamp@gmail.com>
2024-05-26 22:43:59 +0300

    telega 12

--- check_image.py
+++ check_image.py
@@ -1,10 +1,10 @@
 from telebot import types
 import os
 import telebot
+import re
 
 bot = telebot.TeleBot("7016202494:AAEutxmMaJuTvAJS184seuOFgKVF3lxyWUs")
 list_managers = ["mooslyaka"]
-
 
 
 def check_man(message):
@@ -25,6 +25,17 @@ def start(message):
                          reply_markup=markup)
 
 
+def remove_coord(strok):
+    with open('all_coordinates.txt') as f:
+        lines = f.readlines()
+    pattern = re.compile(re.escape(strok))
+    with open('all_coordinates.txt', 'w') as f:
+        for line in lines:
+            result = pattern.search(line)
+            if result is None:
+                f.write(line)
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
     if call.message:
@@ -33,12 +44,14 @@ def callback(call):
                         str(call.data).split()[4])
         if str(call.data).split()[0] == "0":
             os.remove(f'{str(call.data).split()[1]}')
+            remove_coord(str(call.data).split()[1])
 
 
-def check_photo(message, file, longitude, latitude, year, time):
+def check_photo(message, file, longitude, latitude, time):
     image = open(file, "rb")
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Одобрить", callback_data=f"1 {longitude} {latitude} {year} {time}"))
+    markup.add(types.InlineKeyboardButton("Одобрить",
+                                          callback_data=f"1 {longitude} {latitude} {time}"))
     markup.add(types.InlineKeyboardButton("Отклонить", callback_data=f"0 {file}"))
     bot.send_photo(message.chat.id, photo=image, reply_markup=markup)
 
@@ -56,7 +69,7 @@ def text(message):
         if list_coords:
             for i in list_coords:
                 i = i.split()
-                check_photo(message, i[0], i[1], i[2], {i[3]}, {i[4]})
+                check_photo(message, i[0], i[1], i[2], f"{i[3]} {i[4]}")
 
 
 bot.polling(none_stop=True)
